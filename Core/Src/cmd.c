275b041502f3a2a030aaab0bd8957c96fddfd0d8
WNDKS\wndks <wndkswndks222@gmail.com>
2026-05-31 10:49:04 +0900

    기존pc 커밋 테스트

--- Core/Src/cmd.c
+++ Core/Src/cmd.c
@@ -637,7 +637,7 @@ void strstr_n(const char *str, char *strDst ,uint16_t idx, int n)
 
 }
 //
-void Passing_Rx_Gateway()
+void Passing_Rx_Gateway()//
 {
 	if(HAL_GetTick() - m_Gcmd.timeStamp>10 && m_Gcmd.timeStamp)
 	{
